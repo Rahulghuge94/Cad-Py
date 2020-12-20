f1cb295e45934a9bf90357f2701169c0647dd8c3
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-20 12:52:16 +0530

    Update autocad-py.py

--- autocad-py/autocad-py.py
+++ autocad-py/autocad-py.py
@@ -1,14 +1,24 @@
-from pyautocad import Autocad
-from pyautocad.types import APoint
-acad=Autocad()
-ms=acad.doc.ModelSpace
-utility=acad.doc.Utility
-ps=acad.doc.PaperSpace
+#from pyautocad import Autocad
+#from pyautocad.types import APoint
+#acad=Autocad()
+#ms=acad.doc.ModelSpace
+#utility=acad.doc.Utility
+#ps=acad.doc.PaperSpace
 
 
 
-class polyline:
-    def __init__(self, object):
-        self.object=object
-    def add():
-        
+
+def addlwpolyline(VerticesList):       
+    return object.AddLightWeightPolyline(VerticesList)
+def area(object):
+    return object.Area
+def length(object):
+    return object.Length
+def closed(object):
+    return object.Closed
+def setconstantwidth(object):
+    object.ConstantWidth
+def coordinate(object,index)
+    return object.Coordinate(index)
+def coordinates(object)
+    return object.Coordinates
